929f9765a487505ff6ca9c786fb13f80755aefbc
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-02-25 19:03:29 -0500

    setup for train

--- convert_to_jpg.py
+++ convert_to_jpg.py
@@ -6,6 +6,7 @@ for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
     if file_ext == ".png":
         #os.remove(f)
-        # im = Image.open(f)
-        # rgb_im = im.convert('RGB')
-        # rgb_im.save(file_name + '.jpg')
+        im = Image.open(f)
+        rgb_im = im.convert('RGB')
+        rgb_im.save(file_name + '.jpg')
+        os.remove(f)
